f98a2546cd0611dbd25dae7b500f5d0e101e54f9
MacimeG <maxime.glaneux@gmail.com>
2021-07-28 13:11:13 +0200

    Update gateau.js
    j'ai rajouté un commentaire

--- gateau.js
+++ gateau.js
@@ -220,7 +220,7 @@ const jaime = document.querySelectorAll('.like')
 const input = document.querySelectorAll('.liksys')
 console.log(jaime)
 
-
+// Ici je fais mon "systeme"de like. nextElementSibling et pour selectionné le prochain element suivant celui cliqué.
 for (let index = 0; index < jaime.length; index++) {
     const element = jaime[index];
   
