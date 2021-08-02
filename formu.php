fb4607f68f893a9cb37d82864fff5b7bb1750c60
MacimeG <maxime.glaneux@gmail.com>
2021-08-02 16:33:11 +0200

    Update formu.php
    j'ai fais un peu de php.. je galre

--- formu.php
+++ formu.php
@@ -1,3 +1,17 @@
 <?php
-echo "hello wooooooorld"; 
+ print_r($_POST);
+//  if(isset($_POST['username']) &&!empty($_POST['username']))
+     
+//     echo htmlspecialchars($_POST['username']);
+
+
+//  if(isset($_POST['prenom']) &&!empty($_POST['prenom']))
+     
+//     echo htmlspecialchars($_POST['prenom']);
+
+
+//  if(isset($_POST['email']) &&!empty($_POST['email']))
+     
+//     echo htmlspecialchars($_POST['email']);
+ 
 ?>
