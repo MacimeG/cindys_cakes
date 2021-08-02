9f1931c365eb3429a7f8a62a06ca4e00fa26cc9a
MacimeG <maxime.glaneux@gmail.com>
2021-08-02 10:29:11 +0200

    Update gateau.js
    juste quelque truc en js

--- gateau.js
+++ gateau.js
@@ -4,6 +4,11 @@ const NAV = document.querySelector('.menuNavbar')
 const catProd = document.querySelector('.miniNav')
 const menuProd= document.querySelector('.mininavCategori')
 
+// const inputNom = document.querySelector('.Nom')
+// const inputPrenom = document.querySelector('.Prenom')
+// const inputMail = document.querySelector('.Email')
+
+// console.log(inputNom.value)
 
 MENU.addEventListener('click', function(){
     console.log('tu as bien cliqué sur le mainmenu')
