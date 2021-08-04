9e3d1e6042ad7e3aea00d78c63a023c0863e3d33
MacimeG <maxime.glaneux@gmail.com>
2021-08-04 10:42:05 +0200

    php
    j'ai fais un peu de php, histoire de comprendre comment bien BIEN recupéré les données de formulaire

--- formu.php
+++ formu.php
@@ -1,17 +1,22 @@
 <?php
- print_r($_POST);
-//  if(isset($_POST['username']) &&!empty($_POST['username']))
+ if(isset($_POST['username']) &&!empty($_POST['username']))
      
-//     echo htmlspecialchars($_POST['username']);
+    echo htmlspecialchars($_POST['username']);
 
 
-//  if(isset($_POST['prenom']) &&!empty($_POST['prenom']))
+
+ if(isset($_POST['prenom']) &&!empty($_POST['prenom']))
      
-//     echo htmlspecialchars($_POST['prenom']);
+    echo htmlspecialchars($_POST['prenom']);
 
 
-//  if(isset($_POST['email']) &&!empty($_POST['email']))
+ if(isset($_POST['email']) &&!empty($_POST['email']))
      
-//     echo htmlspecialchars($_POST['email']);
+    echo htmlspecialchars($_POST['email']);
+
+
+ if(isset($_POST['textarea']) &&!empty($_POST['textarea']))
+     
+    echo htmlspecialchars($_POST['textarea']);
  
 ?>
