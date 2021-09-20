d88603daa525ca896d8b5e1475b38caed1440863
MacimeG <maxime.glaneux@gmail.com>
2021-09-20 15:55:28 +0200

    php
    declaration de variable

--- formu.php
+++ formu.php
@@ -1,22 +1,28 @@
 <?php
- if(isset($_POST['username']) &&!empty($_POST['username']))
+$username =  htmlspecialchars($_POST['username']);
+$name =  htmlspecialchars($_POST['name']);
+$mail =  htmlspecialchars($_POST['email']);
+$area =  htmlspecialchars($_POST['textarea']);
+
+
+//  if(isset($_POST['username']) &&!empty($_POST['username']))
      
-    echo htmlspecialchars($_POST['username']);
+//     echo htmlspecialchars($_POST['username']);
 
 
 
- if(isset($_POST['prenom']) &&!empty($_POST['prenom']))
+//  if(isset($_POST['prenom']) &&!empty($_POST['prenom']))
      
-    echo htmlspecialchars($_POST['prenom']);
+//     echo htmlspecialchars($_POST['prenom']);
 
 
- if(isset($_POST['email']) &&!empty($_POST['email']))
+//  if(isset($_POST['email']) &&!empty($_POST['email']))
      
-    echo htmlspecialchars($_POST['email']);
+//     echo htmlspecialchars($_POST['email']);
 
 
- if(isset($_POST['textarea']) &&!empty($_POST['textarea']))
+//  if(isset($_POST['textarea']) &&!empty($_POST['textarea']))
      
-    echo htmlspecialchars($_POST['textarea']);
+//     echo htmlspecialchars($_POST['textarea']);
  
 ?>
