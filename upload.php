6277889b3c3aa9ccde5a1700a525de06e4c1e786
MacimeG <maxime.glaneux@gmail.com>
2021-10-15 11:29:58 +0200

    Update upload.php
    décommenté l'upload

--- upload.php
+++ upload.php
@@ -3,31 +3,44 @@ include './connexionDB.php';
 
 $uploads_dir_image = './IMAGE_MAQUETTE';
 $uploads_dir_video = './videoGateau/video';
+
 var_dump($_FILES['photo']);
 // exit;
 
 
-// $tmp_name = $_FILES['photo']['tmp_name'];
-// $name = $_FILES['photo']['name'];
+$tmp_name = $_FILES['photo']['tmp_name'];
+$name = $_FILES['photo']['name'];
 
-// move_uploaded_file($tmp_name, "$uploads_dir_image/$name");
+move_uploaded_file($tmp_name, "$uploads_dir_image/$name");
 // move_uploaded_file($tmp_name, "$uploads_dir_video/$name");
 
-// header('location: ./backCake.php');
+header('location: ./backCake.php');
 
 
-// $requeteDescription = "INSERT INTO `gateau`( `nom`, `description`, `categorie`) VALUES (?, ?, ?)"; 
+$requeteDescription = "INSERT INTO `gateau`( `nom`, `description`, `categorie`) VALUES (?, ?, ?)"; 
 
 
 
-// $prep = $database->prepare($requeteDescription);
-// $prep->execute(array($_POST['nomCake'],$_POST['descriptionCake'], $_POST['categorie']));
+$prep = $database->prepare($requeteDescription);
+$prep->execute(array($_POST['nomCake'],$_POST['descriptionCake'], $_POST['categorie']));
 
-// $requeteDescription = "INSERT INTO `multimedia`(`photo`, `video`, `gateau_id`) VALUES (?, ?, (SELECT id FROM gateau ORDER BY id DESC LIMIT 1))";
+$requeteDescription = "INSERT INTO `multimedia`(`photo`, `video`, `gateau_id`) VALUES (?, ?, (SELECT id FROM gateau ORDER BY id DESC LIMIT 1))";
 
 // INSERT INTO `multimedia`(`photo`, `video`, `gateau_id`) VALUES ('test','test', (SELECT id FROM gateau ORDER BY id DESC LIMIT 1))
 // $regex= '/video*/';
 // $regex2 = '/photo*/';
+
+
+
+
+
+
+
+
+
+
+
+
 
 // if($_FILES['photo']['type'] == 'video/mp4'){
 
