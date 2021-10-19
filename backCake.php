cd38d763eb25400db089f73651734464edec6192
MacimeG <maxime.glaneux@gmail.com>
2021-10-19 16:27:21 +0200

    preview
    preview a finir en JS vanilla

--- backCake.php
+++ backCake.php
@@ -13,7 +13,7 @@ if(!isset($_SESSION['verif'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./backO.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script> -->
     <title>CINDY'S CAKES</title>
 </head>
 <body>
--- upload.php
+++ upload.php
@@ -5,32 +5,18 @@ include './connexionDB.php';
 $uploads_dir_image = './IMAGE_MAQUETTE';
 $uploads_dir_video = './videoGateau/video';
 
-var_dump($_FILES['photo']);
-// exit;
 
+var_dump($_FILES['photo']);
 
 $tmp_name = $_FILES['photo']['tmp_name'];
 $name = $_FILES['photo']['name'];
+// exit;
 
 
 
 
-// header('location: ./backCake.php');
 
-
-// $requeteDescription = "INSERT INTO `gateau`( `nom`, `description`, `categorie`) VALUES (?, ?, ?)"; 
-
-// $prep = $database->prepare($requeteDescription);
-// $prep->execute(array($_POST['nomCake'],$_POST['descriptionCake'], $_POST['categorie']));
-
-// $requeteDescription = "INSERT INTO `multimedia`(`photo`, `video`, `gateau_id`) VALUES (?, ?, (SELECT id FROM gateau ORDER BY id DESC LIMIT 1))";
-
-// INSERT INTO `multimedia`(`photo`, `video`, `gateau_id`) VALUES ('test','test', (SELECT id FROM gateau ORDER BY id DESC LIMIT 1))
-// $regex= '/video*/';
-// $regex2 = '/photo*/';
-
-
-$extensionsvideo = array("video/mp4", "video/mov", "video/avi", "video/flv", "video/wmv");
+$extensionsvideo = array("video/mp4", "video/mov", "video/x-msvideo", "video/flv", "video/wmv");
 $extensionPhoto = array("image/gif", "image/jpeg", "image/png", "image/svg");
 
 if($_FILES['photo']['type'] === "" || $_POST['nomCake'] === "" ||  $_POST['descriptionCake'] == ""){
@@ -65,22 +51,3 @@ else{
     header('location: ./backCake.php');
 }
 
-// ici la requete et la boucle pour faire afficher toute les photos de ma base de données. CSS a faire 
-
-
-// $requeteGetPhoto = "SELECT photo FROM multimedia";
-
-// $prepa = $database->prepare($requeteGetPhoto);
-// $prepa->execute();
-
-// $arrayPhoto = $prepa->fetchAll(PDO::FETCH_OBJ);
-
-// var_dump($arrayPhoto);
-// foreach ($arrayPhoto as $key => $value) {
-//     foreach ($value as  $newValue) {
-//         echo "<img width='100px' src='".$newValue."' alt='' >";
-//     }
-// }
-
-// header('location: backCake.php');
-// Requete pour supprimer les photos, ou video, mais avant sa un peu de CSS coté front et meme coté back 
