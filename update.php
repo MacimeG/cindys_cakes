aff34e4eacefd970c9dcd0eca62d97b72c902211
MacimeG <maxime.glaneux@gmail.com>
2021-10-21 13:25:28 +0200

    Update update.php
    nothing

--- update.php
+++ update.php
@@ -7,6 +7,7 @@ $id = (int)$_POST['id'];
 
 var_dump($nomGateau,$DescriptionGateau,$categorieGateau,$id);
 $requetteUpdate = "UPDATE `gateau` SET `nom`= ?,`description`= ?,`categorie`= ? WHERE id = ?";
+
 $prepaa = $database->prepare($requetteUpdate);
 $prepaa->execute([$nomGateau,$DescriptionGateau,$categorieGateau,$id]);
 header('location: ./modif.php');
