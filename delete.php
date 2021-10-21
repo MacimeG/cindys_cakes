e64cc41d1ef41c0a50c2efd46c28aa34e3222ace
MacimeG <maxime.glaneux@gmail.com>
2021-10-21 09:54:14 +0200

    Update delete.php
    amelioration delete

--- delete.php
+++ delete.php
@@ -4,8 +4,15 @@ include './connexionDB.php';
   var_dump($_GET['id']);
 $requeteDelete = "DELETE FROM `multimedia` WHERE gateau_id = ?";
 
-$prepp = $database->prepare($requeteDelete);
 
+
+$prepp = $database->prepare($requeteDelete);
 $prepp->execute([$_GET['id']]);
 
+
+
+
+$requeteDelete2 = "DELETE FROM `gateau` WHERE id = ?";
+$prepaa = $database->prepare($requeteDelete2);
+$prepaa->execute([$_GET['id']]);
 header('location: ./backCake.php');
