c9e01f91d3a035b47374eb00d21aa13d24ffb3f9
MacimeG <maxime.glaneux@gmail.com>
2021-11-02 10:41:53 +0100

    Update uploadfichier.js

--- uploadfichier.js
+++ uploadfichier.js
@@ -1,21 +1,52 @@
 // $ = DocumentQuerySelector 
 // change et un ecouteur d'évenement.
-// $("input[data-preview]").change(function() {
-// 	var $input = $(this);
-// 	var fileReader = new FileReader();
-// 	fileReader.readAsDataURL(this.files[0]);
-// 	fileReader.onload = function(fileEvent) {
-// 		$($input.data('preview')).attr('src', fileEvent.target.result);
-// 	};
-//     console.log('kiki');
-// });
-
-document.querySelector("input[data-preview]").addEventListener('change', (e) =>{
-	var $input = console.log(this.files);
+$("input[data-preview]").change(function() {
+	var $input = $(this);
 	var fileReader = new FileReader();
 	fileReader.readAsDataURL(this.files[0]);
 	fileReader.onload = function(fileEvent) {
-	document.querySelector($input.data('preview')).setAttribute('src', fileEvent.target.result);
+		$($input.data('preview')).attr('src', fileEvent.target.result);
 	};
-})
+    console.log('kiki');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelector("input[data-preview]").addEventListener('change', (e) =>{
+// 	var $input = console.log(this.files);
+// 	var fileReader = new FileReader();
+// 	fileReader.readAsDataURL(this.files[0]);
+// 	fileReader.onload = function(fileEvent) {
+// 	document.querySelector($input.data('preview')).setAttribute('src', fileEvent.target.result);
+// 	};
+// })
 
