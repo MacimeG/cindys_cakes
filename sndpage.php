b732d31804d90df88117b560c5055dc84f10b8ec
MacimeG <maxime.glaneux@gmail.com>
2021-11-04 12:00:14 +0100

    Update sndpage.php
    supp comm

--- sndpage.php
+++ sndpage.php
@@ -81,7 +81,6 @@ include './vueSndPage.php';
                                 <img src="./IMAGE_MAQUETTE/NoPath - Copie (6).png" alt="" class="like">
                                <span class="liksys">0</span>
                             </div>
-                                <!-- <img src="./IMAGE_MAQUETTE/NoPath - Copie (26).png" alt="" class="commentaire" width="28px"> -->
                         </div>
                     </section>
                     </div> 
